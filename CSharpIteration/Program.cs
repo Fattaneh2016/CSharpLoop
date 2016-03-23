2d9d76affe22728986227606d4215d0a34fe41fa
Fattaneh2016 <fatameri@hotmail.com>
2016-03-23 18:44:13 -0400

    Add XO to console

--- CSharpIteration/Program.cs
+++ CSharpIteration/Program.cs
@@ -10,6 +10,36 @@ namespace CSharpIteration
     {
         static void Main(string[] args)
         {
+            Char myChar = 'O';
+
+            for (var i = 0; i < 8; i++)
+            {
+                for (var x = 0; x < 8; x++)
+                {
+                    myChar = getChar(myChar);
+
+                    Console.Write(myChar);
+                }
+
+                myChar = getChar(myChar);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        static Char getChar(Char theChar)
+        {
+            if (theChar == 'X')
+            {
+                return 'O';
+            }
+            else
+            {
+                return 'X';
+            }
         }
     }
 }
+
